5399158f56665f7bb88cd1f940fedae9a2d41a3b
mitisit89 <mitisit89@gmail.com>
2022-08-26 09:54:07 +0300

    upd

--- main.py
+++ main.py
@@ -1,36 +1,22 @@
-import functools
+import itertools
 import pathlib
-import re
-from typing import TextIO, Union
 
 
-@functools.lru_cache
-def parcer(path_to_file: Union[str, pathlib.Path]):
-    try:
-        with open(path_to_file, "r", encoding="utf-8") as file, open(
-            "eng.txt", "w"
-        ) as eng_txt, open("ru.txt", "w") as ru_txt:
-            words = {}
-            for line in file:
-                if not line.startswith("#"):
-                    eng = re.sub(r"\t.*", " ", line)
-                    rus = re.findall(re.compile(r"\t.*"), line)
-                    words[f"{eng}"] = "".join(rus)
-            for k, v in words.items():
-                if re.search(";", k):
-                    eng_txt.writelines(re.sub("[;\t]", "\n", k))
-                    ru_txt.writelines(re.sub("[;\t]", "\n", v))
-                elif re.search(";", v):
-                    ru_txt.writelines(re.sub("[;\t]", "\n", v))
-                    eng_txt.writelines(re.sub("[;\t]", "\n", k))
-                else:
-                    ru_txt.writelines(re.sub("[;\t]", "\n", v))
-                    eng_txt.writelines(re.sub("[;\t]", "\n", k))
-
-    except OSError :
-        print("file not found")
-    finally:
-        file.close()
+def parcer(path_to_file: str | pathlib.Path):
+    with open("./PythonTest.txt", "r", encoding="utf-8") as file, open(
+        "eng.txt", "w"
+    ) as eng_txt, open("ru.txt", "w") as ru_txt:
+        try:
+            for line in file.readlines():
+                en, ru = line.strip().split("\t")
+                en = (x.strip() for x in en.split(";"))
+                ru = (x.strip() for x in ru.split(";"))
+                for en_word, ru_word in zip(en, itertools.cycle(ru)):
+                    eng_txt.writelines(f"{en_word}\n")
+                    ru_txt.writelines(f"{ru_word}\n")
+        except FileNotFoundError:
+            print("File not found. Check the path variable and filename")
+            exit()
 
 
 parcer("./PythonTest.txt")
